8b427f51d3ca7c0cde0194d23e03e6a366b60799
Ngo Hoang Hiep <hiepnh@magenest.com>
2021-03-23 11:34:51 +0700

    fix

--- customaddons/sim_car_repair_industry/models/custom_sale.py
+++ customaddons/sim_car_repair_industry/models/custom_sale.py
@@ -179,32 +179,7 @@ class SaleOrderLine(models.Model):
     car_model = fields.Char(string="Model #")
     name = fields.Text(string='Description')
 
-    def _prepare_invoice_line(self):
-        """
-        Prepare the dict of values to create the new invoice line for a sales order line.
-
-        :param qty: float quantity to invoice
-        """
-        self.ensure_one()
-        res = {
-            'license_plate': self.license_plate,
-            'car_model': self.car_model,
-            'display_type': self.display_type,
-            'sequence': self.sequence,
-            'name': self.name,
-            'product_id': self.product_id.id,
-            'product_uom_id': self.product_uom.id,
-            'quantity': self.qty_to_invoice,
-            'discount': self.discount,
-            'price_unit': self.price_unit,
-            'tax_ids': [(6, 0, self.tax_id.ids)],
-            'analytic_account_id': self.order_id.analytic_account_id.id,
-            'analytic_tag_ids': [(6, 0, self.analytic_tag_ids.ids)],
-            'sale_line_ids': [(4, self.id)],
-        }
-        if self.display_type:
-            res['account_id'] = False
-        return res
+   
 
 
 class AccountInvoiceLine(models.Model):
